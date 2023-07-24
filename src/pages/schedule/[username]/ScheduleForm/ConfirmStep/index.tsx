4c188332fc88eaf9ac13f78f743ffad7b56b3e34
mateus-cristian <mateus_cristian@outlook.com>
2023-07-24 19:54:52 -0300

    feat:validation fields

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -3,15 +3,21 @@ import { ConfirmForm, FormActions, FormError, FormHeader } from './styles'
 import { Button, Text, TextArea, TextInput } from '@redshiftui/react'
 import { CalendarBlank, Clock } from 'phosphor-react'
 import { z } from 'zod'
-import { useForm } from 'react-hook-form'
+import { Controller, useFieldArray, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import dayjs from 'dayjs'
 import { useRouter } from 'next/router'
 import { api } from '../../../../../lib/axios'
+import { toast, Toast, Toaster } from 'react-hot-toast'
+
+const participantSchema = z.object({
+    name: z.string().min(3, 'No mínimo 3 caracteres').nullable(),
+    email: z.string().email("Digite um email válido").nullable(),
+});
+
 
 const confirmFormSchema = z.object({
-    name: z.string().min(3, 'No minímo 3 caracteres'),
-    email: z.string().email("Digite um email válido"),
+    participants: z.array(participantSchema),
     observations: z.string().nullable()
 })
 
@@ -26,25 +32,56 @@ export default function ConfirmStep({ schedulingDate, onCancelConfirmationStep }
 
     const {
         register,
+        control,
         handleSubmit,
+        setError,
         formState: { errors, isSubmitting }
     } = useForm<ConfirmFormData>({
         resolver: zodResolver(confirmFormSchema)
     })
+
+
+    const { fields, append, remove } = useFieldArray<ConfirmFormData>({ control, name: 'participants' })
 
     const router = useRouter();
     const username = String(router.query.username)
 
     async function handleConfirmScheduling(data: ConfirmFormData) {
 
-        const { email, name, observations } = data;
+        const { participants, observations } = data;
+
+        if (participants.length === 0) {
+            toast.error('Deve haver pelo menos um participante')
+            return;
+        }
+
+
+        if (participants.length > 0) {
+            participants.forEach((item, index) => {
+                if (!item.name) {
+                    setError(`participants.${index}.name`, {
+                        type: 'required',
+                        message: 'Campo obrigatório'
+                    });
+                }
+
+                if (!item.email) {
+                    setError(`participants.${index}.email`, {
+                        type: 'required',
+                        message: 'Campo obrigatório'
+                    });
+                }
+                return;
+            })
+        }
+
 
         await api.post(`users/${username}/schedule`, {
-            name,
-            email,
+            participants,
             observations,
             date: schedulingDate
         })
+        toast.success('Reunião marcada com sucesso')
 
         onCancelConfirmationStep()
     }
@@ -54,6 +91,14 @@ export default function ConfirmStep({ schedulingDate, onCancelConfirmationStep }
 
     return (
         <ConfirmForm as='form' onSubmit={handleSubmit(handleConfirmScheduling)}>
+            <Toaster
+                position='top-center'
+                toastOptions={{
+                    style: {
+                        fontFamily: 'Roboto'
+                    }
+                }}
+            />
             <FormHeader>
                 <Text>
                     <CalendarBlank />
@@ -65,18 +110,55 @@ export default function ConfirmStep({ schedulingDate, onCancelConfirmationStep }
                     {describeTime}
                 </Text>
             </FormHeader>
+            <Button type='button' onClick={() => {
+                append({ name: null, email: null })
+            }
+            }>Adicionar convidado</Button>
+            {
+                fields.map((item, index) => {
+                    return (
+                        <div key={item.id}>
+                            <Text size="sm" style={{ paddingBottom: 16, fontSize: 16, fontWeight: 'bold' }}>Convidado {index + 1}:</Text>
+                            <label>
+                                <Text size="sm">Nome completo</Text>
+                                <Controller
+                                    name={`participants.${index}.name`}
+                                    control={control}
+                                    render={(({ field }) => (
+                                        <>
+                                            <TextInput placeholder='Nome' onChange={(value) => field.onChange(value)} />
+                                            {errors?.participants?.[index]?.name && (
+                                                <FormError>{errors.participants?.[index]?.name?.message}</FormError>
+                                            )}
+                                        </>
+                                    ))}
+                                />
+                            </label>
 
-            <label>
-                <Text size="sm">Nome completo</Text>
-                <TextInput placeholder='seu nome' {...register('name')} />
-                {errors.name && (<FormError>{errors.name.message}</FormError>)}
-            </label>
-
-            <label>
-                <Text size="sm">Endereço de email</Text>
-                <TextInput placeholder='email@com.br'  {...register('email')} />
-                {errors.email && <FormError>{errors.email.message}</FormError>}
-            </label>
+                            <label style={{ marginTop: 16 }}>
+                                <Text size="sm">Endereço de email</Text>
+                                <Controller
+                                    name={`participants.${index}.email`}
+                                    control={control}
+                                    render={(({ field }) => (
+                                        <>
+                                            <TextInput placeholder='Email' onChange={(value) => field.onChange(value)} />
+                                            {errors?.participants?.[index]?.email && (
+                                                <FormError>{errors.participants?.[index]?.email?.message}</FormError>
+                                            )}
+                                        </>
+                                    ))}
+                                />
+                            </label>
+                            <div>
+                                <Button style={{ marginTop: 16 }} type='button' onClick={() => {
+                                    remove(index)
+                                }}>remover convidado {index + 1}</Button>
+                            </div>
+                        </div>
+                    )
+                })
+            }
 
             <label>
                 <Text size="sm">Observações</Text>
@@ -87,7 +169,7 @@ export default function ConfirmStep({ schedulingDate, onCancelConfirmationStep }
                 <Button type='button' variant="tertiary" onClick={onCancelConfirmationStep}>Cancelar</Button>
                 <Button type='submit' disabled={isSubmitting}>Confirmar</Button>
             </FormActions>
-        </ConfirmForm>
+        </ConfirmForm >
     )
 }
 
